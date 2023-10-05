b6ac6066cb57953bf985fd4c552951f81b84ca05
sammcdo <sam_mcdo@yahoo.com>
2023-10-05 10:54:16 -0400

    readme generator

---
+++ create_main_page.py
@@ -0,0 +1,52 @@
+import os
+
+from autokattis import Kattis
+from dotenv import load_dotenv
+import shutil
+
+load_dotenv(".env")
+
+desc = """
+These are problems I have solved on [Kattis](https://open.kattis.com)
+while preparing for competitive programming competitions.
+
+"""
+
+table = """
+<table>
+<th>
+<td>Name</td>
+<td>Difficulty</td>
+<td>Link</td>
+</th>
+"""
+
+tableFormat = """
+<tr>
+<td>{name}</td>
+<td>{difficulty}</td>
+<td>{link}</td>
+</tr>
+"""
+
+# Log in to kattis
+kt = Kattis(os.environ.get("K_USER"), os.environ.get("K_PASS"))
+
+with open("README.md", 'w') as obj:
+    obj.write("# King-McD Kattis Problems\n")
+
+    obj.write(desc)
+
+    kt.plot_problems(filepath="plot.png", show_partial=False)
+    obj.write("### Problem Difficulty\n")
+
+    obj.write("![](plot.png)")
+
+    problems = kt.problems(show_partial=False)
+
+    obj.write(table)
+    for p in problems:
+        # p["name"] = p["name"].decode('utf-8')
+        obj.write(tableFormat.format(**p))
+
+
